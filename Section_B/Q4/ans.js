9a7a0c7f4dcbc35bfce0e349c0dc00c7f09887cb
Monjila-Moni <you@example.com>
2025-05-18 00:54:28 +0600

    final

--- Section_B/Q4/ans.js
+++ Section_B/Q4/ans.js
@@ -1,18 +1,18 @@
 // Q:write a js function that removes duplicates from an array of integers
 
-function removeDuplicates(arr){
-    return [...new Set(arr) ]
-}
-const numbers = [1, 2, 3, 3, 4, 4, 5];
-const uniqueNumbers = removeDuplicates(numbers);
-
-console.log(uniqueNumbers)
-
-
-// function removeDuplicates(arr) {
-//   return arr.filter((value, index) => arr.indexOf(value) === index);
+// function removeDuplicates(arr){
+//     return [...new Set(arr) ]
 // }
-
-// const numbers = [1, 7, 2, 3, 4, 4, 5];
+// const numbers = [1, 2, 3, 3, 4, 4, 5];
 // const uniqueNumbers = removeDuplicates(numbers);
-// console.log(uniqueNumbers); 
+
+// console.log(uniqueNumbers)
+
+
+function removeDuplicates(arr) {
+  return arr.filter((value, index) => arr.indexOf(value) === index);
+}
+
+const numbers = [1, 7, 2, 3, 4, 4, 5];
+const uniqueNumbers = removeDuplicates(numbers);
+console.log(uniqueNumbers); 
